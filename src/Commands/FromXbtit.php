f902999268538a4496c02a25852671621256db8d
pxgamer <owzie123@gmail.com>
2017-12-15 16:50:22 +0000

    Redone the main FromXbtit class

--- src/Commands/FromXbtit.php
+++ src/Commands/FromXbtit.php
@@ -2,11 +2,13 @@
 
 namespace pxgamer\XbtitToUnit3d\Commands;
 
+use App\Torrent;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Database\Capsule\Manager;
-use pxgamer\XbtitToUnit3d\Models\Torrent;
-use pxgamer\XbtitToUnit3d\Models\User;
-use pxgamer\XbtitToUnit3d\Models\UserInfo;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class FromXbtit extends Command
 {
@@ -16,19 +18,21 @@ class FromXbtit extends Command
      * @var string
      */
     protected $signature = 'unit3d:from-xbtit
-                            {--driver=mysql : The XBTIT driver type to use (mysql, sqlsrv, etc.).}
-                            {--host=localhost : The XBTIT hostname or IP.}
-                            {--database= : The XBTIT database to select from.}
-                            {--username= : The XBTIT mysql user.}
-                            {--password= : The XBTIT mysql password.}
-                            {--prefix= : The XBTIT hostname or IP.}';
+                            {--driver=mysql : The driver type to use (mysql, sqlsrv, etc.).}
+                            {--host=localhost : The hostname or IP.}
+                            {--database= : The database to select from.}
+                            {--username= : The database user.}
+                            {--password= : The database password.}
+                            {--prefix= : The database hostname or IP.}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import data from a XBTIT instance to UNIT3D.';
+    protected $description = 'Import data from an XBTIT instance to UNIT3D.';
+
+    protected $results = [];
 
     /**
      * Create a new command instance.
@@ -48,56 +52,103 @@ class FromXbtit extends Command
      */
     public function handle()
     {
-        $capsule = $this->getCapsule();
+        $this->checkRequired($this->options());
 
-        if (!$capsule->schema('xbtit')->hasTable('xbtit_users')) {
-            throw new \ErrorException('XBTIT user tables missing.');
-        }
-
-        $users = new User($capsule);
-        $users->importAll();
-
-        if (!$capsule->schema('xbtit')->hasTable('xbtit_files')) {
-            throw new \ErrorException('XBTIT torrent tables missing.');
-        }
-
-        $torrent = new Torrent($capsule);
-        $torrent->importAll();
-    }
-
-    private function getCapsule()
-    {
-        $capsule = new Manager();
-
-        $capsule->addConnection(
-            [
+        config([
+            'database.connections.imports' => [
                 'driver'    => $this->option('driver'),
                 'host'      => $this->option('host'),
                 'database'  => $this->option('database'),
                 'username'  => $this->option('username'),
                 'password'  => $this->option('password'),
-                'charset'   => 'utf8',
-                'collation' => 'utf8_unicode_ci',
                 'prefix'    => $this->option('prefix'),
-            ],
-            'xbtit'
-        );
-
-        $capsule->addConnection(
-            [
-                'driver'    => env('DB_CONNECTION', 'mysql'),
-                'host'      => env('DB_HOST', '127.0.0.1'),
-                'port'      => env('DB_PORT', 3306),
-                'database'  => env('DB_DATABASE', 'unit3d'),
-                'username'  => env('DB_USERNAME', 'root'),
-                'password'  => env('DB_PASSWORD', 'root'),
                 'charset'   => 'utf8',
                 'collation' => 'utf8_unicode_ci',
-            ]
-        );
+            ],
+        ]);
 
-        $capsule->setAsGlobal();
+        $database = DB::connection('imports');
 
-        return $capsule;
+        $this->importTable($database, 'User', 'users', User::class);
+        $this->importTable($database, 'Torrent', 'torrents', Torrent::class);
+    }
+
+    protected function importTable(Connection $database, string $type, string $oldTable, string $modelName)
+    {
+        if (!$database->getSchemaBuilder()->hasTable($oldTable)) {
+            throw new \ErrorException('`'.$oldTable.'` table missing.');
+        }
+
+        $oldData = $database->query()->select()->from($oldTable)->get();
+
+        foreach ($oldData->all() as $oldDataItem) {
+            $data = $this->map($type, $oldDataItem);
+
+            $this->results[$oldTable] = $this->import($modelName, $data);
+        }
+    }
+
+    public function map(string $type, \stdClass $data): array
+    {
+        return $this->{'map'.$type}($data);
+    }
+
+    public function mapUser(\stdClass $data): array
+    {
+        return [
+            'username'   => $data->username,
+            'password'   => $data->password,
+            'passkey'    => $data->salt,
+            'group_id'   => $data->id_level,
+            'email'      => $data->email,
+            'uploaded'   => $data->uploaded,
+            'downloaded' => $data->downloaded,
+            'image'      => $data->avatar,
+            'created_at' => Carbon::createFromTimestamp(strtotime($data->joined)),
+            'last_login' => Carbon::createFromTimestamp(strtotime($data->lastconnect)),
+        ];
+    }
+
+    public function mapTorrent(\stdClass $data): array
+    {
+        return [
+            'info_hash'   => $data->info_hash,
+            'name'        => $data->filename,
+            'size'        => $data->size,
+            'announce'    => $data->announce_url,
+            'description' => $data->comment,
+            'user_id'     => $data->uploader,
+            'seeders'     => $data->seeds,
+            'leechers'    => $data->leechers,
+            'created_at'  => Carbon::createFromTimestamp(strtotime($data->data)),
+            'updated_at'  => Carbon::createFromTimestamp(strtotime($data->lastupdate)),
+        ];
+    }
+
+    private function import(string $model, array $data = []): bool
+    {
+        /** @var Model $new */
+        $new = new $model();
+
+        foreach ($data as $item => $value) {
+            $new->$item = $value;
+        }
+
+        return $new->save();
+    }
+
+    private function checkRequired(array $options)
+    {
+        $requiredOptions = [
+            'database',
+            'username',
+            'password',
+        ];
+
+        foreach ($requiredOptions as $option) {
+            if (!key_exists($option, $options) || !$options[$option]) {
+                throw new \InvalidArgumentException('Option `'.$option.'` not provided.');
+            }
+        }
     }
 }
